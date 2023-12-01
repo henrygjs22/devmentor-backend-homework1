f4d2b59553a55f7e2394b5aac9a264bf9bc914c6
henry <henrygjs22@gmail.com>
2023-12-01 21:54:07 +0800

    refactor: DI BookCourseEvent

--- src/Zhtw.php
+++ src/Zhtw.php
@@ -6,10 +6,13 @@ class Zhtw
 {
     public function getMessage(): array
     {
-        return [
-            "RegisterSuccessfulEvent" => " 註冊成功 ",
-            "BookCourseEvent" => " 預約課程 ",
-            "CancelCourseEvent" => " 取消課程 "
-        ];
+        return json_decode(file_get_contents(__DIR__ . '/zhtw.json'), true);
+
+
+        // return [
+        //     RegisterSuccessfulEvent::class => " 註冊成功 ",
+        //     "BookCourseEvent" => " 預約課程 ",
+        //     "CancelCourseEvent" => " 取消課程 "
+        // ];
     }
 }
